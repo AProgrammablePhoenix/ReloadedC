a8025611c4be399b5249827da9b196010c92b872
AProgrammablePhoenix <alexgf27550@gmail.com>
2023-07-12 15:41:19 +0200

    Uncommented some functions declarations since pointers are now supported.

--- resources/stdio.h
+++ resources/stdio.h
@@ -1,7 +1,7 @@
-//__nativedecl(vlibc) int remove(const char* filename);
-//__nativedecl(vlibc) int rename(const char* old_name, const char* new_name);
+__nativedecl(vlibc) int remove(const char* filename);
+__nativedecl(vlibc) int rename(const char* old_name, const char* new_name);
 //__nativedecl(vlibc) FILE* tmpfile();
-//__nativedecl(vlibc) char* tmpnam(char* str);
+__nativedecl(vlibc) char* tmpnam(char* str);
 //__nativedecl(vlibc) int fclose(FILE* stream);
 //__nativedecl(vlibc) int fflush(FILE* stream);
 //__nativedecl(vlibc) FILE* fopen(const char* filename, const char* mode);
@@ -18,7 +18,7 @@
 //__nativedecl(vlibc) int fputs(const char* str, FILE* stream);
 __nativedecl(vlibc) int getchar();
 __nativedecl(vlibc) int putchar(int ch);
-//__nativedecl(vlibc) int puts(const char* str);
+__nativedecl(vlibc) int puts(const char* str);
 //__nativedecl(vlibc) int ungetc(int ch, FILE* stream):
 //__nativedecl(vlibc) long ftell(FILE* stream);
 //__nativedecl(vlibc) int fgetpos(FILE* stream, fpos_t* pos);
